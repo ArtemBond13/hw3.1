5801c89861f84eb1d121328f6e7072f8ddc67c04
Artem <garribond85@gmail.com>
2021-01-23 17:38:05 +0500

    added Import() method

--- pkg/transaction/transaction.go
+++ pkg/transaction/transaction.go
@@ -3,6 +3,8 @@ package transaction
 import (
 	"encoding/csv"
 	"io"
+	"log"
+	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -54,6 +56,7 @@ func (s *Service) Export(writer io.Writer) error {
 			transaction.Id,
 			transaction.From,
 			transaction.To,
+			strconv.FormatInt(transaction.Amount, 10),
 			strconv.FormatInt(transaction.Created, 10),
 		}
 		records = append(records, record)
@@ -62,4 +65,74 @@ func (s *Service) Export(writer io.Writer) error {
 	w := csv.NewWriter(writer)
 	return w.WriteAll(records)
 
+}
+
+// Чтение файла
+//func (s *Service) Import(reader io.Reader) error {
+//	file, err := os.Open("export.scv")
+//	if err != nil{
+//		log.Println(err)
+//	}
+//
+//	defer func(c io.Closer) {
+//		if cerr := c.Close(); cerr != nil{
+//			log.Println(err)
+//			if err == nil {
+//				err = cerr
+//			}
+//		}
+//	}(file)
+//
+//	// Срез для хранения содержимот
+//	content := make([]byte, 0)
+//
+//	// буфер для чтения
+//	buf := make([]byte, 4096) // 4096 - количство байт
+//
+//	for {
+//		n, err := file.Read(buf)
+//		if err != nil {
+//			// io.EOF - ошибка, сигнализирующая о том, что дочитали данные до конца (файл закончился)
+//			if err != io.EOF {
+//				log.Println(err)
+//			}
+//			// "перекладываем" данные из буфера в слайс со всем содержимым
+//			content = append(content, buf[:n]...)
+//			break
+//		}
+//		content = append(content, buf[:n]...)
+//	}
+//
+//}
+
+
+func (s *Service) Import(r io.Reader) error {
+	file, err := os.Open("export.scv")
+	if err != nil{
+		log.Println(err)
+	}
+
+	defer func(c io.Closer) {
+		if cerr := c.Close(); cerr != nil{
+			log.Println(err)
+			if err == nil {
+				err = cerr
+			}
+		}
+	}(file)
+
+	reader := csv.NewReader(file)
+	records := make([][]string, 0)
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err != io.EOF {
+				log.Println(err)
+			}
+			records = append(records, record)
+			break
+		}
+		records = append(records, record)
+	}
+	return nil
 }
